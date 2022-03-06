d01164f11b1ac0c19bb2aee08ddf2048a97bb56a
Abdulfetah Jemal <32792371+Abdulfetahjemal@users.noreply.github.com>
2022-03-06 21:37:31 +0300

    Lab 1

---
+++ Lab1/Exersise.sql
@@ -0,0 +1,19 @@
+
+-- Question 1 Solution : Create database Query   
+CREATE DATABASE DBMS
+CREATE DATABASE HILCOE
+
+
+--  Question 2 Solution :   Use Database Query  
+USE DBMS
+USE HILCOE
+
+
+-- Question 3 Solution : Rename database query 
+ALTER DATABASE  DBMS MODIFY NAME = DB
+
+
+-- Question 4 Solution : Delete database Query 
+
+USE MASTER
+DROP DATABASE HILCOE
